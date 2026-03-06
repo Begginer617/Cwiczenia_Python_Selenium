c4d4e5eaa70466cec38afa0fca9ffa40445ef01e
Milosz Skorupski <skorupskimilosz@gmail.com>
2026-03-06 01:55:32 +0100

    erased test from booking, added test with verifing driver operation

--- Pages/BookingPage.py
+++ Pages/BookingPage.py
@@ -3,26 +3,24 @@ from Pages.BasePage import BasePage
 
 class BookingPage(BasePage):
 
-    # Nagłówek strony rezerwacji – używany do sprawdzenia, czy strona się załadowała.
-    HEADER = (By.XPATH, '//h2[contains(text(), "Book this room")]')
+    # Nagłówek modala rezerwacji.
+    # Modal pojawia się po kliknięciu "Book this room".
+    HEADER = (By.XPATH, '//div[contains(@class, "modal")]//h2')
 
-    # Pole "First name" w formularzu.
+    # Pole "First name" w formularzu modala.
     FIRST_NAME = (By.XPATH, '//input[@id="firstname"]')
 
-    # Przycisk "Submit" wysyłający formularz.
+    # Przycisk "Submit" w modalu.
     SUBMIT_BUTTON = (By.XPATH, '//button[contains(text(), "Submit")]')
 
-    # Sprawdza, czy strona rezerwacji jest widoczna.
-    # Używa is_visible() z BasePage.
+    # Sprawdza, czy modal rezerwacji jest widoczny.
     def is_loaded(self):
         return self.is_visible(self.HEADER)
 
     # Wpisuje imię do pola "First name".
-    # Używa type() z BasePage.
     def fill_first_name(self, name):
         self.type(self.FIRST_NAME, name)
 
     # Kliknięcie przycisku "Submit".
-    # Używa click() z BasePage.
     def submit(self):
         self.click(self.SUBMIT_BUTTON)
